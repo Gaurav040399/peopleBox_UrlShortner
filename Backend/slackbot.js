d655f5b02df638951627815a15f4a4013efb70be
Gaurav Asode <gauravasode5@gmail.com>
2024-02-20 20:42:09 +0530

    url shortner

---
+++ Backend/slackbot.js
@@ -0,0 +1,23 @@
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  
+  port: process.env.PORT || 3000
+});
+
+
+app.message('hello', async ({ message, say }) => {
+  
+  await say(`Hey there <@${message.user}>!`);
+});
+
+(async () => {
+  
+  await app.start();
+
+  console.log('⚡️ Bolt app is running!');
+})();
---
+++ Frontend/js/home.js
@@ -0,0 +1,42 @@
+const shortenbtn = document.getElementById("btn");
+
+let url = "https://ga-us.onrender.com";
+// let url = "http://localhost:7070"
+
+shortenbtn.addEventListener("click", () => {
+    let urlinp = document.getElementById("url");
+    // console.log(urlinp);
+
+    // Construct the payload object
+    const requestBody = {
+        fullUrl: urlinp.value 
+    };
+
+    fetch(`${url}/url/shorten`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": localStorage.getItem("token")
+        },
+        body: JSON.stringify(requestBody) // Sending the object as JSON
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        if(data.message === "URL already present"){
+            
+            document.getElementById("output").innerText = `Short URL : ${url}/url/${data.shortUrl}`
+        }else{
+            document.getElementById("output").innerText = `Short URL : ${url}/url/${data.shortUrl}`
+        }
+    })
+    .catch(err => console.error(err));
+});
+
+
+const logoutbtn = document.getElementById("logoutbtn")
+
+logoutbtn.addEventListener("click",()=>{
+    localStorage.clear();
+    window.location.href = "http://127.0.0.1:5500/Frontend/index.html"
+})
